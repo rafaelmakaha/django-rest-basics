7f7d9c818fdf902977407fb0e6a00f902a3a6042
Rafael Makaha Gomes Ferreira <rafael.makaha@gmail.com>
2018-12-17 18:16:17 -0200

    Adds Views to Users and Groups.
    Signed-off-by: Rafael Makaha Gomes Ferreira <rafael.makaha@gmail.com>

--- quickstart/views.py
+++ quickstart/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from .serializers import UserSerializer, GroupSerializer
 
-# Create your views here.
+class UserViewSet (viewsets.ModelViewSet):
+    # Allows user to be viewed or edited
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+class GroupViewSet (viewsets.ModelViewSet):
+    # Allow groups to be viewed or edited
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
